0a51a6185e9d0b4b839621619f9d927f49c04653
Christian <christiantisby1@gmail.com>
2018-07-22 18:16:36 -0400

    First commit

---
+++ src/com/example/example2/Example2.java
@@ -0,0 +1,25 @@
+package com.example.example2;
+
+/*
+
+    This demonstrates a variable.
+
+    Call this file Example2.java
+
+ */
+
+public class Example2 {
+    public static void main(String[] args) {
+        int var1; //  this declares a variable
+        int var2; //  this declares another variable
+
+        var1 = 1024;  //  this assigns 1024 to var1
+
+        System.out.println("var1 contains " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.println("var 2 contains var1 / 2: ");
+        System.out.println(var2);
+    }
+}
